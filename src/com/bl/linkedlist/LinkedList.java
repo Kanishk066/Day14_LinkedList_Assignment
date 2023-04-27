9d2e1458184c8f9f8be61ee0ccd060e9ace7837e
Kanishk <kanishksinghal06@gmail.com>
2023-04-27 09:27:24 +0530

    Added the code for simple link list

--- src/com/bl/linkedlist/LinkedList.java
+++ src/com/bl/linkedlist/LinkedList.java
@@ -1,8 +1,47 @@
 package com.bl.linkedlist;
+class LinkedListNode{
+    int value;
+    LinkedListNode Next;
+    public LinkedListNode(int value){
+        this.value = value;
+        this.Next = null;
+    }
+}
+  class LinkedList1{
+    LinkedListNode head;
+    public LinkedList1(){
+        this.head = null;
+    }
+    public void add(int value){
+        LinkedListNode newNode = new LinkedListNode(value);
+        if (head == null) {
+            head = newNode;
+        }
+        else {
+            LinkedListNode current = head;
 
+            while(current.Next != null) {
+                current = current.Next;
+            }
+                current.Next = newNode;
+        }
+    }
+    public void printList(){
+                LinkedListNode current = head;
+                while (current != null) {
+                    System.out.println(" " + current.value);
+                    current = current.Next;
+                }
+    }
+}
 public class LinkedList {
     public static void main(String[] args) {
         System.out.println("Welcome To the Linked List Problem");
+        LinkedList1 list = new LinkedList1();
+        list.add(56);
+        list.add(30);
+        list.add(70);
+        list.printList();
     }
 }
 
