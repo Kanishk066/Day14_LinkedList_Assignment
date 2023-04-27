ab885d8f8799e1d3781f1df12daa1b353b647328
Kanishk <kanishksinghal06@gmail.com>
2023-04-27 16:11:03 +0530

    Added the code for Appending the list

--- src/com/bl/linkedlist/LinkedList.java
+++ src/com/bl/linkedlist/LinkedList.java
@@ -7,50 +7,69 @@ class LinkedListNode{
         this.Next = null;
     }
 }
-  class LinkedList1{
-    LinkedListNode head;
-    public LinkedList1(){
-        this.head = null;
-    }
-    public void add(int value){
-        LinkedListNode newNode = new LinkedListNode(value);
-        if (head == null) {
-            head = newNode;
-        }
-        else {
-            LinkedListNode current = head;
+  class LinkedList1 {
+      LinkedListNode head;
 
-            while(current.Next != null) {
-                current = current.Next;
-            }
-                current.Next = newNode;
-        }
-    }
-    public void addInfront(int value){
-        LinkedListNode newNode = new LinkedListNode(value);
-        if (head == null) {
-            head = newNode;
-        } else {
-            newNode.Next = head;
-            head = newNode;
-        }
-    }
-    public void printList(){
-                LinkedListNode current = head;
-                while (current != null) {
-                    System.out.println(" " + current.value);
-                    current = current.Next;
-                }
-    }
-}
-public class LinkedList {
-    public static void main(String[] args) {
-        System.out.println("Welcome To the Linked List Problem");
-        LinkedList1 list = new LinkedList1();
-        list.addInfront(56);
-        list.addInfront(30);
-        list.addInfront(70);
-        list.printList();
-    }
-}
+      public LinkedList1() {
+          this.head = null;
+      }
+
+      public void add(int value) {
+          LinkedListNode newNode = new LinkedListNode(value);
+          if (head == null) {
+              head = newNode;
+          } else {
+              LinkedListNode current = head;
+
+              while (current.Next != null) {
+                  current = current.Next;
+              }
+              current.Next = newNode;
+          }
+      }
+
+      public void addInfront(int value) {
+          LinkedListNode newNode = new LinkedListNode(value);
+          if (head == null) {
+              head = newNode;
+          } else {
+              newNode.Next = head;
+              head = newNode;
+          }
+      }
+
+      public void append(int value) {
+          LinkedListNode newNode = new LinkedListNode(value);
+
+          if (head == null) {
+              head = newNode;
+          } else {
+              LinkedListNode current = head;
+
+              while (current.Next != null) {
+                  current = current.Next;
+              }
+
+              current.Next = newNode;
+          }
+      }
+          public void printList() {
+              LinkedListNode current = head;
+              while (current != null) {
+                  System.out.println(" " + current.value);
+                  current = current.Next;
+              }
+          }
+      }
+
+      public class LinkedList {
+          public static void main(String[] args) {
+              System.out.println("Welcome To the Linked List Problem");
+              LinkedList1 list = new LinkedList1();
+              list.add(56);
+              list.append(30);
+              list.append(70);
+              list.printList();
+          }
+      }
 
