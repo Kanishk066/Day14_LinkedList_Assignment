2fcfdbaab9a5026063bcb198eec2fa6721df9100
Kanishk <kanishksinghal06@gmail.com>
2023-04-29 19:57:37 +0530

    Refactor the code to create Sorted Link List

--- src/com/bl/linkedlist/LinkedList.java
+++ src/com/bl/linkedlist/LinkedList.java
@@ -1,103 +1,55 @@
 package com.bl.linkedlist;
-class LinkedListNode{
-    int value;
-    LinkedListNode Next;
-    public LinkedListNode(int value){
-        this.value = value;
-        this.Next = null;
+class SortedLinkedList<T extends Comparable<T>> {
+     Node<T> head;
+     int size;
+    public void add(T data) {
+        Node<T> newNode = new Node<>(data);
+        Node<T> current = head, prev = null;
+        while (current != null && current.data.compareTo(data) < 0) {
+            prev = current;
+            current = current.next;
+        }
+        if (prev == null) {
+            head = newNode;
+        } else {
+            prev.next = newNode;
+        }
+        newNode.next = current;
+        size++;
+    }
+    public int size() {
+        return size;
+    }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Node<T> current = head;
+        while (current != null) {
+            sb.append(current.data).append("->");
+            current = current.next;
+        }
+        sb.append("null");
+        return sb.toString();
+    }
+    private static class Node<T extends Comparable<T>> {
+        T data;
+        Node<T> next;
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
     }
 }
-  class LinkedList1 {
-      LinkedListNode head;
-
-      public LinkedList1() {
-          this.head = null;
-      }
-      public void insertAfter(LinkedListNode prevNode, int newValue){
-          if (prevNode == null) {
-              System.out.println("The given previous node cannot be null");
-              return;
-          }
-          LinkedListNode newNode = new LinkedListNode(newValue);
-          newNode.Next = prevNode.Next;
-          prevNode.Next = newNode;
-      }
-      public void add(int value) {
-          LinkedListNode newNode = new LinkedListNode(value);
-          if (head == null) {
-              head = newNode;
-          } else {
-              LinkedListNode current = head;
-
-              while (current.Next != null) {
-                  current = current.Next;
-              }
-              current.Next = newNode;
-          }
-      }
-      public void insert(int newValue) {
-          LinkedListNode newNode = new LinkedListNode(newValue);
-          if (head == null) {
-              head = newNode;
-              return;
-          }
-          newNode.Next = null;
-          LinkedListNode last = head;
-          while (last.Next != null) {
-                  last = last.Next;
-              }
-          last.Next = newNode;
-      }
-
-      public void deleteNode(int key) {
-          LinkedListNode temp = head, prev = null;
-          if (temp != null && temp.value == key) {
-              head = temp.Next;
-              return;
-          }
-          while (temp != null && temp.value != key) {
-              prev = temp;
-              temp = temp.Next;
-          }
-          if (temp == null) {
-              return;
-          }
-          prev.Next = temp.Next;
-      }
-      public int size(){
-          LinkedListNode currNode = head;
-          int count = 0;
-          while(currNode != null) {
-              count++;
-              currNode = currNode.Next;
-          }
-          return count;
-      }
-      public void printList() {
-              LinkedListNode current = head;
-              while (current != null) {
-                  System.out.println(" " + current.value);
-                  current = current.Next;
-              }
-          }
-      }
       public class LinkedList {
           public static void main(String[] args) {
               System.out.println("Welcome To the Linked List Problem");
-              LinkedList1 list = new LinkedList1();
-              list.insert(56);
-              list.insert(30);
-              list.insert(40);
-              list.insert(70);
-              System.out.println("Original");
-              list.printList();
-
-              int key = 40;
-              list.deleteNode(key);
-              System.out.println("\nAfter Deleting Node with data 40");
-              list.printList();
-
-              System.out.println("\nSize of LinkedList: " + list.size());
+              SortedLinkedList list = new SortedLinkedList();
+             list.add(56);
+             list.add(30);
+             list.add(40);
+             list.add(70);
+              System.out.println(list);
+              System.out.println("Size: " +list.size());
           }
       }
 
